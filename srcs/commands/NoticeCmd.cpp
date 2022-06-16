044b8a936cfd297c562e51327ad46f69457aba19
Byrd Betsey <bbetsey12@gmail.com>
2022-06-16 15:46:17 +0400

    feat(commands): noticecmd fixed

--- srcs/commands/NoticeCmd.cpp
+++ srcs/commands/NoticeCmd.cpp
@@ -13,17 +13,39 @@ NoticeCmd::~NoticeCmd(void) {}
 
 // MARK: - Class Methods
 
-void	NoticeCmd::execute(void) {
-	if (args.size() != 4)
-		return ;
-	
-	std::string nickname = args.at(1);
-	std::string msg = args.at(2);
+std::string	NoticeCmd::getTopic(std::vector<std::string> &args) {
+	std::vector<std::string>::iterator it;
+	std::string topic;
 
-	User *userTarget = usersList->getUser(nickname);
+	it += 2;
+	if ((*it)[0] == '#')
+		(*it).erase(0, 1);
+	while (it != args.end()) {
+		topic += (*it);
+		if (it + 1 != args.end())
+			topic += " ";
+		++it;
+	}
+	return topic;
+}
+
+void	NoticeCmd::execute(void) {
+	std::string target = args.at(1);
+	std::string msg = getTopic(args);
+
+	if (target.at(0) == '#') {
+		Channel *channel = channelsList->getChannel(target);
+		if (channel)
+			channel->mailing(":" + user->getNickname() + " NOTICE " + target + " :" + msg, user);
+		else
+			user->appendMessage(":server " + std::string(ERR_NOSUCHCHANNEL) + " " + target + " : No such channel");
+		return ;
+	}
+
+	User *userTarget = usersList->getUser(target);
 	if (userTarget) {
-		userTarget->appendMessage(":" + user->getNickname() + " NOTICE " + nickname + " :" + msg);
+		userTarget->appendMessage(":" + user->getNickname() + " NOTICE " + target + " :" + msg);
 	} else {
-		user->appendMessage(":server " + std::string(ERR_NOSUCHNICK) + " " + nickname + " : No such nick/channel");
+		user->appendMessage(":server " + std::string(ERR_NOSUCHNICK) + " " + target + " : No such nick/channel");
 	}
 }
--- srcs/commands/NoticeCmd.hpp
+++ srcs/commands/NoticeCmd.hpp
@@ -12,6 +12,7 @@ class NoticeCmd : public Command
 		~NoticeCmd(void);
 
 		void execute(void);
+		std::string getTopic(std::vector<std::string> &args);
 };
 
 #endif
